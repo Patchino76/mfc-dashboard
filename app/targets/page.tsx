faeb879c7515e5ebc553f94c969fd8de3785645a
Svetoslav Lyubenov <patchino76@gmail.com>
2024-10-30 16:27:41 +0200

    fixed the api with using dataframes

--- app/targets/page.tsx
+++ app/targets/page.tsx
@@ -3,9 +3,11 @@
 import { Flex } from "@radix-ui/themes";
 import LinearGaugeWithTarget from "../components/LinearGaugeWithTarget";
 import { useState } from "react";
+import { usePulseTrend } from "../hooks/usePulse";
 
 export default function targetsPage() {
-  const [temperature, setTemperature] = useState(22.5);
+  const { data } = usePulseTrend({ tags: "RECOVERY_LINE1_CU_LONG,RECOVERY_LINE2_CU_LONG", num_records: 5 });
+  console.log(data);
   return (
     <Flex gap="3" direction="column" mt={"3"}>
       <LinearGaugeWithTarget />
